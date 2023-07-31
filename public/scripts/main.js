e1f757108a9d1f4a4158ccad4b3fb148a168f9ef
rhit-mohlerad <112584374+rhit-mohlerad@users.noreply.github.com>
2023-07-31 12:56:18 -0700

    auth working. No signout button yet, so use browser console

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,34 +3,114 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Ryan Jin, Alexander Mohler
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_REQUESTS = "Requests";
+rhit.FB_COLLECTION_OFFERS = "Offers";
+rhit.FB_COLLECTION_USERS = "Users";
+rhit.FB_KEY_COMMENT = "comment";
+rhit.FB_KEY_DRIVER = "driver";
+//add more keys
+rhit.fbAuthManager = null;
+//add other managers
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+// from: https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro/35385518#35385518
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-rhit.ClassName = class {
+rhit.LoginPageController = class {
 	constructor() {
-
-	}
-
-	methodName() {
-
+		document.querySelector("#rosefireButton").onclick = (event) => {
+			rhit.fbAuthManager.signIn();
+		}
 	}
 }
+
+rhit.FbAuthManager = class {
+	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+	signIn() {
+		Rosefire.signIn("f19089c3-ba70-4635-aae5-fced7a139f0f", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+
+			// TODO: Use the rfUser.token with your server.
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				if (errorCode === 'auth/invalid-custom-token') {
+					alert('the token you provided is not valid.');
+				} else {
+					console.error("Custom auth error", errorCode, errorMessage);
+				}
+			});
+		});
+
+	}
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			console.log("sign out error");
+		});
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+	get uid() {
+		return this._user.uid;
+	}
+}
+
+rhit.initializePage = function () {
+	if (document.querySelector("#loginPage")) {
+		console.log("login page");
+		new rhit.LoginPageController();
+	}
+};
+
+rhit.checkForRedirects = function () {
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/home.html";
+	}
+
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
+	}
+};
+
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	rhit.fbAuthManager = new rhit.FbAuthManager();
+	rhit.fbAuthManager.beginListening(() => {
+		console.log("auth change callback fired. TODO: check for redirects and init the page");
+		console.log("isSignedIn = ", rhit.fbAuthManager.isSignedIn);
+
+		rhit.checkForRedirects();
+
+		rhit.initializePage();
+	});
 };
 
 rhit.main();
