634280b0e2099bbfdd016fabf44a7038039ee943
rhit-jinrc <112568279+rhit-jinrc@users.noreply.github.com>
2023-08-07 20:12:09 -0400

    profile pic upload (not linked to firebase data yet)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -845,6 +845,23 @@ rhit.checkForRedirects = function () {
 
 	
 };
+//----------------------------- Profile Picture Upload -----------------------------
+//Mostly rough test code that I will move later into a controller and manager
+window.addEventListener('load', function() {
+	document.querySelector('input[type="file"]').addEventListener('change', function() {
+		if (this.files && this.files[0]) {
+			var img = document.querySelector('img');
+			img.onload = () => {
+				URL.revokeObjectURL(img.src);  // no longer needed, free memory
+			}
+  
+			img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+
+			//write code here to set the firebase database information
+		}
+	});
+  });
+
 
 
 //----------------------------- Google Maps API -----------------------------
